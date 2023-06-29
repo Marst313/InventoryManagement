24dbc1dbadd118198f5b104ebc3b965641586e2a
I Wayan Bayu Arya <bayiabayu@gmail.com>
2023-06-29 19:50:48 +0700

    fixing typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
     <section className="bg-primary h-[90vh] w-full">
       <h1>Home</h1>
       <h2>mamah cantik</h2>
+      <h2>papah ganteng</h2>
     </section>
   );
 };
