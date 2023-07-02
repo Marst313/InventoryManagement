b8322d54fde55d6ec2c68eb0b0acd0c59fce7ef1
NdaruWindra <ndaruw29@gmail.com>
2023-07-03 01:54:48 +0700

    Nambahin gambar dan button selengkapnya di home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ImageHome from "../assets/images/Image-Home.png";
+import IconSelengkapnya from "../assets/icons/icon-selengkapnyaHome.png";
 
 const Home = () => {
   return (
@@ -8,7 +10,7 @@ const Home = () => {
           <h1 className="text-[64px] font-rajhdani font-bold text-[#FFFF] ">
             Kelola Inventaris Barang dengan Mudah dan Efisien
           </h1>
-          <p className="font-rajhdani text-[14px] text-[#FFFF]">
+          <p className="font-rajhdani text-[14px] text-[#FFFF] leading-[42px]">
             Selamat datang di{" "}
             <span className="text-[25px] font-bold text-[#FF6000]">
               INVENT PRO
@@ -18,11 +20,12 @@ const Home = () => {
             alat yang Anda butuhkan untuk menjaga persediaan barang Anda tetap
             terorganisir, efisien, dan terkendali.
           </p>
-          <button class="rounded-full color bg-orange-500 p-2 w-[20%] my-5 text-[16px] text-[#FFFF] ">
+          <button class="rounded-full color bg-orange-500 p-2 w-[22%] my-5 text-[16px] text-[#FFFF] flex  ">
             <span>Selengkapnya</span>
+            <img src={IconSelengkapnya} alt="icon" className="w-6 h-7 mx-2" />
           </button>
         </div>
-
+        <img src={ImageHome} alt="Image-Home" className="w-[750px] h-[540px]" />
         <div>
           <div></div>
         </div>
